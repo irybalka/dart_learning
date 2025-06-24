fe863f9d38428fa744d4247258943618c8fcabef
Igor Rybalka <irybalka@yahoo.com>
2025-06-24 19:31:56 +0300

    feat(DART-06): added arithmetic operations

--- l_task.dart
+++ l_task.dart
@@ -1,2 +1,10 @@
 void main() {
+  double width = 18.4;
+  double height = 30.9;
+  double area = width * height;
+  double perimeter = 2 * (width + height);
+  int squares = area ~/ 1;
+  print("The area of the rectangle is $area");
+  print("The perimeter of the rectangle is $perimeter");
+  print("The number of squares is $squares");
 }
