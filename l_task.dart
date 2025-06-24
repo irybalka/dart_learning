daa89c09b50aeef9cf9747a4000afe7333e79591
Igor Rybalka <irybalka@yahoo.com>
2025-06-24 21:57:06 +0300

    feat(DART-06): function Comparison() was added

--- l_task.dart
+++ l_task.dart
@@ -1,4 +1,5 @@
 void main() {
+  //arithmetic operations
   double width = 18.4;
   double height = 30.9;
   double area = width * height;
@@ -7,4 +8,23 @@ void main() {
   print("The area of the rectangle is $area");
   print("The perimeter of the rectangle is $perimeter");
   print("The number of squares is $squares");
+
+
+  //comparison
+  double number = 771.0;
+  print(Comparison(number));
+
+}
+
+String Comparison(var number) {
+  if (number is! num) {
+    return "Input is not a number";
+  }
+
+  String comparison50 = number > 50 ? "Greater than 50" : "Not greater than 50";
+  String comparison100 = number < 100 ? "Less than 100" : "Not less than 100";
+  String devision5     = number % 5  == 0 ? "Divisible by 5" : "Not divisible by 5";
+  return "$comparison50," + " $comparison100," + " $devision5";
+
+
 }
